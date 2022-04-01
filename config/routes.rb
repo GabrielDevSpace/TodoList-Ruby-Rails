dff70a44165866aa5b1e900923822d77604614cf
GabrielPires <gabrielpirestj@hotmail.com>
2022-03-31 23:22:28 -0300

    Criado Rota / como index do todolist

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :todo_lists do
     resources :todo_items
+   
   end
+  root to: "todo_lists#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
